6f9b4f2a8dd60d909ca699c6b841346052ec2a16
Kirill Veni <VeniVidiVigorY@yandex.ru>
2024-07-05 21:13:52 +0300

    Number 3 commit

--- homework-advanced-py/main.py
+++
@@ -1,9 +0,0 @@
-import datetime
-from application.salary import calculate_salary
-from application.db.people import get_employees
-
-
-if __name__ == '__main__':
-    print(datetime.date.today())
-    calculate_salary()
-    get_employees()
